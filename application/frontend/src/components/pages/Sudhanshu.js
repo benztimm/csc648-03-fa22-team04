eb37e6d7db01e046aa7fe10610f78e75e0242b9f
Sudhanshu Kulkarni <sudhanshu.1.k@gmail.com>
2022-09-17 02:14:12 -0700

    Sudhanshu's about me page

--- application/frontend/src/components/pages/Sudhanshu.js
+++ application/frontend/src/components/pages/Sudhanshu.js
@@ -20,7 +20,10 @@ const Sudhanshu = () => {
         </div>
         <div className="about">
           <h1>About Me</h1>
-          Insert your about here
+          Sudhanshu here, all the way from India for Master's in Computer Science here at SFSU. This is my first semester here.
+          I have completed my bachelors in Computer Engineering from International Institute of Information Technology in Pune, India. Computers have always fascinated me because I believe technology can make world a better place, but we really need to understand what makes it a bad place to begin with.
+          Before coming here at San Francisco, I have been working at a tech-based startup in India, as a full stack developer for couple of years. Every domain of technology is immensely interesting for me, and I love to explore and learn new things, which is my main motivation for joining this class.
+          Besides classes, I love to travel and hike. I am a great fan of music mostly Indie and Rock genres, and love to watch movies as well. Absolutely love to play badminton and cricket, and also enjoy pc gaming.
         </div>
       </div>
     </div>
