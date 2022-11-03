9ca27ca2ec52ade595d0522b9202d9c6f9d0b523
schu-code <sophiachu2@gmail.com>
2022-11-02 23:10:52 -0700

    Added json parsing in SearchResults

--- application/frontend/src/components/pages/SearchResults.js
+++ application/frontend/src/components/pages/SearchResults.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './styles/milestoneOne.css';
 import ResultCard from './ResultCard';
 
 function SearchResults() {
-  return (
-    <div className='wrapper'>
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    </div>
-  );
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    console.log(items);
+    return (
+      
+      <div className='wrapper'>
+        <ResultCard/>
+      </div>
+    );
+  }
 }
 
 
 
+
 export default SearchResults;
+
+{/* <ul>
+{items.map(item => (
+  <li key={item.id}>
+    {item.name} {item.price}
+  </li>
+))}
+</ul> */}
