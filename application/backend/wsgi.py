dbf71f4e802bd19da02b82d09501edf532fcab71
Benzekarat <benzekarat@Ekarats-MacBook-Pro.local>
2022-11-17 17:04:46 -0800

    change port 5000 -> 8000

--- application/backend/wsgi.py
+++ application/backend/wsgi.py
@@ -6,4 +6,4 @@ from routes import *
 # As flask is not a production suitable server, we use will
 # a WSGIServer instance to serve our flask application. 
 if __name__ == '__main__':  
-    WSGIServer(('0.0.0.0', 5000), app).serve_forever()
+    WSGIServer(('0.0.0.0', 8000), app).serve_forever()
