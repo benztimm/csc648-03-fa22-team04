072cd22482c079cff1bc562e09c10a063e49124f
Jerry <jliu36@mail.sfsu.edu>
2022-12-01 13:35:32 -0800

    fixed login() when verifying encrypted password with user input

--- application/backend/apis/auth.py
+++ application/backend/apis/auth.py
@@ -38,7 +38,8 @@ def login():
         _email = _json['email']
         _password = _json['password']
         if _email and _password and request.method == 'POST':
-            query = """
+            # find email from user input
+            query1 = """
                 SELECT
                     u.user_email as 'user_email'
                 FROM 
@@ -46,12 +47,22 @@ def login():
                 WHERE
                     user_email = %(user_email)s
                 """
+            # find encrypted password from user email
+            query2 = """
+                SELECT 
+                    u.user_password as 'user_password'
+                FROM
+                    user u 
+                WHERE
+                    user_email = %(user_email)s
+                """
             data = _email
-            auth_email = db.execute_query(query, data)
-            if auth_email and _password == decode(_password):
-                session['loggedin'] = True
-                session['id'] = auth_email['id']
-                session['username'] = auth_email['username']
+            auth_email = db.execute_query(query1, data)
+            auth_password = db.execute_query(query2, data)
+            # check if user input matches stored email and decoded password
+            if auth_email and _password == decode(auth_password):
+                session['logged'] = True
+                session['email'] = auth_email['email']
                 # Redirect to home page
                 return 'Logged in successfully!'
             else:
@@ -62,16 +73,18 @@ def login():
 
 
 def decode(user_password):
+    # decode encrypted password from database
+    # used to decrypt stored password and then check if it matches password from user input when logging in
     query = """
     SELECT 
-        u.user_password as 'user_password' 
+        u.user_password as 'password' 
     FROM 
         user u
     WHERE
-        user_password = %(user_password)s
+        user_email = %(user_email)s
 
         """
-    results = db.execute_query(query, user_password)
+    results = db.execute_query(query)
 
     for user_password in results:
         try:
