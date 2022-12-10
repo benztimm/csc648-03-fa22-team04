e353221287bbe16ffaf573d26a66f859f938af81
Ekarat Buddharuksa <ebuddharuksa@sfsu.edu>
2022-12-09 18:56:21 -0800

    remove app.config['UPLOAD_FOLDER']

--- application/backend/root.py
+++ application/backend/root.py
@@ -10,4 +10,3 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['UPLOAD_FOLDER'] = "../../posts"
