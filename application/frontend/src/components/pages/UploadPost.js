76d2c70b7fc497ba1cdb3f6082fa1f378ecafe67
schu-code <sophiachu2@gmail.com>
2022-12-16 02:02:45 -0800

    Upload testing

--- application/frontend/src/components/pages/UploadPost.js
+++ application/frontend/src/components/pages/UploadPost.js
@@ -7,7 +7,7 @@ Description: Page for users to upload files and enter file's information/price
 
 */
 
-import React, { useState } from 'react';
+import { useContext, useRef, useState, useEffect } from 'react';
 import './styles/forms.css';
 import Modal from './modal';
 import { Link, useNavigate, useHistory, generatePath, useLocation } from 'react-router-dom';
@@ -21,111 +21,146 @@ function UploadPost() {
     const [isRedirectOpen, setRedirectOpen] = useState(false);
     const [isLoginOpen, setLoginOpen] = useState(false);
     const [isRegisterOpen, setRegisterOpen] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    const uploader_id = useRef(null);
+    const post_type = useRef(null);
+    const title = useRef(null);
+    const description = useRef();
+    const price = useRef(null);
+    const category = useRef(null);
+    const fileInput = useRef(null);
+
     const navigate = useNavigate();
+
+
+
+
+
+
+
+    useEffect(() => {
+        fetch(`http://54.200.101.218:5000/get-category/`)
+            .then(response => response.json())
+            .then(data => setCategories(data.output));
+    }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        const title = event.target.title.value;
-        const mtype = event.target.mtype.value;
-        const category = event.target.category.value;
-        const description = event.target.description.value;
+        const data = new FormData();
+        data.append('file', fileInput.current.files[0]);
+        data.append('post_type', post_type);
+        data.append('title', title);
+        data.append('description', description);
+        data.append('price', price);
         if (event.target.price.value === null) {
-            const price = 0;
+            data.append('price', 0);
         } else {
-            const price = event.target.price.value;
+            data.append('price', price);
         }
+        data.append('category', category);
 
-        const file = event.target.file.value;
 
         if (sessionStorage.getItem('user') === null) {
             setLoginOpen(true);
-        } else if (title && mtype && category && file) {
+        } else {
+            const user = JSON.parse(sessionStorage.getItem("user"));
+            const user_id = user.user.user_id;
+            data.append('uploader_id', user_id);
+            fetch('http://54.200.101.218:5000/upload_file/', {
+                method: 'POST',
+                body: data,
+              }).then((response) => {
+                response.json();
+                console.log(response);
+              });
+            }
             setRedirectOpen(true);
         }
+    
+
+
+
+    const toDashboard = () => {
+        navigate('/dashboard');
+
     }
+    //sessionStorage.getItem('user') !== null
+    //{isLoginOpen && <Login setLoginOpen={setLoginOpen} setRegisterOpen={setRegisterOpen} setUserLogin={setUserLogin}/>}
 
-
-
-const toDashboard = () => {
-    navigate('/dashboard');
-
-}
-//sessionStorage.getItem('user') !== null
-//{isLoginOpen && <Login setLoginOpen={setLoginOpen} setRegisterOpen={setRegisterOpen} setUserLogin={setUserLogin}/>}
-
-return (
-    <div className='upload-form-container'>
-        <div className='upload-forms'>
-            <div className='upload-header'>
-                <h1>UPLOAD YOUR ITEM</h1>
-                <hr className='line-upload'></hr>
-                <br />
-                <text><i>(All required fields are specified with an </i></text> <text style={{ color: 'red' }}><i>*</i></text> <text><i> )</i></text>
-                <br />
-                <br />
-            </div>
-
-            <form onSubmit={handleSubmit}>
-                <div className='form-container'>
-                    <label className='input-text-field'>
-                        TITLE<a style={{ color: 'red' }}><i>*</i></a><br></br>
-                        <input type="text" name="title" required className="forms" />
-                    </label><br /><br />
-                    <label className='input-text-field'>
-                        MEDIA TYPE<text style={{ color: 'red' }}><i>*</i></text><br></br>
-                        <select name="mtype" required className="forms">
-                            <option value="image">Image</option>
-                            <option value="video">Video</option>
-                            <option value="document">Document</option>I
-                        </select>
-                    </label><br /><br />
-                    <label className='input-text-field'>
-                        CATEGORY<text style={{ color: 'red' }}><i>*</i></text><br></br>
-                        <select name="category" required className="forms">
-                            <option value="computerscience">Computer Science</option>
-                            <option value="photography">Photography</option>
-                            <option value="music">Music</option>I
-                        </select>
-                    </label><br /><br />
-
-                    <label className='input-text-field'>
-                        DESCRIPTION<br />
-                        <textarea name="description" className="forms" rows="4" />
-                    </label><br /><br />
-
-                    <label className='input-text-field'>
-                        PRICE<br />
-                        <input type="number" name="price" className="forms" />
-                    </label><br /><br />
-
-                    <label>
-                        FILE<text style={{ color: 'red' }}><i>*</i></text><br></br>
-                        <input type="file" name="file" required className="forms" />
-                    </label><br /><br />
-
-                    <br /><br />
-                    <div className='upload-note'>Note: May take up to 24hrs to approve the post</div>
+    return (
+        <div className='upload-form-container'>
+            <div className='upload-forms'>
+                <div className='upload-header'>
+                    <h1>UPLOAD YOUR ITEM</h1>
+                    <hr className='line-upload'></hr>
                     <br />
-                    <button className='upload-submit-button' type="submit" >SUBMIT</button>
+                    <text><i>(All required fields are specified with an </i></text> <text style={{ color: 'red' }}><i>*</i></text> <text><i> )</i></text>
+                    <br />
+                    <br />
                 </div>
-            </form>
+
+                <form onSubmit={handleSubmit}>
+                    <div className='form-container'>
+                        <label className='input-text-field'>
+                            TITLE<a style={{ color: 'red' }}><i>*</i></a><br></br>
+                            <input type="text" name="title" ref={title} required className="forms" />
+                        </label><br /><br />
+                        <label className='input-text-field'>
+                            MEDIA TYPE<text style={{ color: 'red' }}><i>*</i></text><br></br>
+                            <select name="type" ref={post_type} required className="forms">
+                                <option value="Image">Image</option>
+                                <option value="Video">Video</option>
+                                <option value="Document">Document</option>I
+                            </select>
+                        </label><br /><br />
+                        <label className='input-text-field'>
+                            CATEGORY<text style={{ color: 'red' }}><i>*</i></text><br></br>
+                            <select name="category" ref={category} required className="forms">
+                                {categories.map((category) => (
+                                    <option value={category.category_id}>{category.category_name}</option>
+                                ))}
+                            </select>
+                        </label><br /><br />
+
+                        <label className='input-text-field'>
+                            DESCRIPTION<br />
+                            <textarea name="description" ref={description} className="forms" rows="4" />
+                        </label><br /><br />
+
+                        <label className='input-text-field'>
+                            PRICE<br />
+                            <input type="number" name="price" ref={price} className="forms" />
+                        </label><br /><br />
+
+                        <label>
+                            FILE<text style={{ color: 'red' }}><i>*</i></text><br></br>
+                            <input type="file" name="file" ref={fileInput} required className="forms" />
+                        </label><br /><br />
+
+                        <br /><br />
+                        <div className='upload-note'>Note: May take up to 24hrs to approve the post</div>
+                        <br />
+                        <button className='upload-submit-button' type="submit" >SUBMIT</button>
+                    </div>
+                </form>
+
+            </div>
+            {isRedirectOpen && <Modal setRedirectOpen={setRedirectOpen}>
+                <div className='upload-modal-text'>
+                    Your submission was successful!<br />
+                    Please allow up to 24 hours for approval.<br />
+                </div>
+                <button className='go-to-uploads' onClick={toDashboard}>Go to My Uploads <AiOutlineArrowRight /></button>
+            </Modal>}
+
+            {isLoginOpen && <Login setLoginOpen={setLoginOpen} setRegisterOpen={setRegisterOpen} />}
+            {isRegisterOpen && <Register setRegisterOpen={setRegisterOpen} setLoginOpen={setLoginOpen} />}
+
 
         </div>
-        {isRedirectOpen && <Modal setRedirectOpen={setRedirectOpen}>
-            <div className='upload-modal-text'>
-                Your submission was successful!<br />
-                Please allow up to 24 hours for approval.<br />
-            </div>
-            <button className='go-to-uploads' onClick={toDashboard}>Go to My Uploads <AiOutlineArrowRight /></button>
-        </Modal>}
-
-        {isLoginOpen && <Login setLoginOpen={setLoginOpen} setRegisterOpen={setRegisterOpen} />}
-        {isRegisterOpen && <Register setRegisterOpen={setRegisterOpen} setLoginOpen={setLoginOpen}/>}
-
-
-    </div>
-);
+    );
 }
 
 
